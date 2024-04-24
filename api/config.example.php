eced61fa05d6241cd16fb73539bb6a92b39d2e08
iuroc <m@iuroc.com>
2024-04-25 02:50:48 +0800

    ...

--- api/config.example.php
+++ api/config.example.php
@@ -1,8 +1,9 @@
 <?php
 
-error_reporting(0);
+// error_reporting(0);
 
 define('MYSQL_HOST', '127.0.0.1');
 define('MYSQL_USER', 'root');
 define('MYSQL_PASS', '');
 define('MYSQL_DB', '');
+define('ROOT_PASSWORD', '');
